29e9fab4c160146ddedf94bb18d1b672bfe9ad84
Javier Lancha Vázquez <javier.lancha@gmail.com>
2022-06-17 21:53:00 +0200

    Add Telescope plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,6 +7,12 @@ require('packer').startup(function()
     -- (or >= 0.5 with the 0.5-compat branch)
     use {'feline-nvim/feline.nvim', branch = 'master'}
 
+    use 'nvim-lua/plenary.nvim'
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+
     -- Nightfox themes:
     -- nightfox, dayfox, dawnfox, duskfox, nordfox, terafox
     use "EdenEast/nightfox.nvim"
