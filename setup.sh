61a30b8b9c9a11302bff1eb4f6fb58cbd04488a3
Javier Lancha Vázquez <javier.lancha@gmail.com>
2022-06-20 01:21:08 +0200

    Add Hack font to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
-sudo apt install npm
-
+# Install patched fonts (icon support)
 echo "[-] Download fonts [-]"
-mkdir .p ~/.fonts
-echo "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip"
+mkdir -p ~/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip
-unzip UbuntuMono.zip -d ~/.fonts
-rm UbuntuMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
+
+unzip UbuntuMono.zip -d ~/.fonts/
+unzip Hack.zip -d ~/.fonts/
+rm UbuntuMono.zip Hack.zip
+
 fc-cache -fv
 echo "done!"
 
+
 # Install packer.nvim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+# git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+#     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
