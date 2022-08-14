98b90dbd343fdbe415fbef1d079177dc07e9af94
Javier Lancha Vázquez <javier.lancha@gmail.com>
2022-08-15 00:42:27 +0200

    Add FiraCode font

--- install-fonts.sh
+++ install-fonts.sh
@@ -3,10 +3,12 @@ echo "[-] Download fonts [-]"
 mkdir -p ~/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip
 
 unzip UbuntuMono.zip -d ~/.fonts/
 unzip Hack.zip -d ~/.fonts/
-rm UbuntuMono.zip Hack.zip
+unzip FiraCode.zip -d ~/.fonts/
+rm UbuntuMono.zip Hack.zip FiraCode.zip
 
 fc-cache -fv
 echo "done!"
