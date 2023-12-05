56d15277f4d1dba1443aef144cb8f21d687a2d88
Javier Lancha Vázquez <javier.lancha@gmail.com>
2023-12-05 19:56:38 +0100

    Update tree-sitter settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -143,8 +143,10 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'lua', 'typescript', 'rust', 'go', 'python' },
+  ensure_installed = { 'c', 'lua', 'rust', 'python' },
 
+  sync_install = false,
+  auto_install = true,
   highlight = { enable = true },
   indent = { enable = true },
   incremental_selection = {
